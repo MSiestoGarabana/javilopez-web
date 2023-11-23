72fa1d92861922a73a551136735f733c75c9dcf6
MSiesto <miguelsiesto@gmail.com>
2023-11-23 15:37:00 +0100

    deleting aboutMe page

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage/HomePage";
-import AboutMePage from "../pages/AboutMePage/AboutMePage";
 import ProjectsPage from "../pages/ProjectsPage/ProjectsPage";
 import SponsorsPage from "../pages/SponsorsPage/SponsorsPage";
 import ContactPage from "../pages/ContactPage/ContactPage";
@@ -10,7 +9,6 @@ const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/AboutMe" element={<AboutMePage />} />
       <Route path="/Proyectos" element={<ProjectsPage />} />
       <Route path="/Sponsors" element={<SponsorsPage />} />
       <Route path="/Contacto" element={<ContactPage />} />
