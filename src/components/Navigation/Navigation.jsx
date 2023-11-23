3f8c70540a568d861625b034b17b598d93562cef
MSiesto <miguelsiesto@gmail.com>
2023-11-23 15:38:14 +0100

    correcting navBar links

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,19 +3,16 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
-import { Link as NavLink } from "@mui/material";
 import { Link } from "react-router-dom";
+import javiLogo from "../../media/javiLogo.jpeg";
+import "./Navigation.css";
 
-const pages = ["Home", "Proyectos", "Sponsors", "Contacto"];
+const pages = ["Proyectos", "Sponsors", "Contacto"];
 
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -29,9 +26,14 @@ function ResponsiveAppBar() {
   };
 
   return (
-    <AppBar position="fixed">
+    <AppBar position="fixed" sx={{ backgroundColor: "white" }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
+          <Box>
+            <Link to="/">
+              <img className="navBar__logo" src={javiLogo} />
+            </Link>
+          </Box>
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
@@ -39,7 +41,7 @@ function ResponsiveAppBar() {
               aria-controls="menu-appbar"
               aria-haspopup="true"
               onClick={handleOpenNavMenu}
-              color="inherit"
+              color="black"
             >
               <MenuIcon />
             </IconButton>
@@ -63,23 +65,13 @@ function ResponsiveAppBar() {
             >
               {pages.map((page) => (
                 <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  {page === "Home" ? (
-                    <Link to="/">{page}</Link>
-                  ) : (
-                    <Link to={`/${page}`}>{page}</Link>
-                  )}
+                  <Link to={`/${page}`}>{page}</Link>
                 </MenuItem>
               ))}
             </Menu>
           </Box>
 
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            <Button sx={{ my: 2, color: "white", display: "block" }}>
-              <Link to="/">Home</Link>
-            </Button>
-            <Button sx={{ my: 2, color: "white", display: "block" }}>
-              <Link to="/AboutMe">About Me</Link>
-            </Button>
             <Button sx={{ my: 2, color: "white", display: "block" }}>
               <Link to="/Proyectos">Proyectos</Link>
             </Button>
@@ -90,8 +82,6 @@ function ResponsiveAppBar() {
               <Link to="/Contacto">Contacto</Link>
             </Button>
           </Box>
-
-          <Box sx={{ flexGrow: 0 }}>sup</Box>
         </Toolbar>
       </Container>
     </AppBar>
