d876181245f117b47044710c8821ddad579e6fe3
MSiesto <miguelsiesto@gmail.com>
2023-11-23 15:37:33 +0100

    implementing aboutMe component

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Carousel from "../../components/Carousel/Carousel";
+import AboutMe from "../../components/AboutMe/AboutMe";
 
 const Home = () => {
   return (
     <div>
       <Carousel />
+      <AboutMe />
     </div>
   );
 };
