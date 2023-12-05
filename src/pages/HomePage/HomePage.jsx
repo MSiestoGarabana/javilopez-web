1c0d3e0dff2eaa94a0223542422c0205100082dc
MSiesto <miguelsiesto@gmail.com>
2023-12-05 18:00:50 +0100

    deleting contactLink from homePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import AboutMe from "../../components/AboutMe/AboutMe";
 import WelcomeComponent from "../../components/WelcomeComponent/WelcomeComponent";
-import ContactLinkComponent from "../../components/ContactLinkComponent/ContactLinkComponent";
 
 const Home = () => {
   return (
     <div>
       <WelcomeComponent />
       <AboutMe />
-      <ContactLinkComponent />
     </div>
   );
 };
