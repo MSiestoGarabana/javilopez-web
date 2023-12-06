badd2f9427e747536d29c3c6888bdf64f014213e
MSiesto <miguelsiesto@gmail.com>
2023-12-06 16:56:53 +0100

    creating & implementing CheckMyProjects Component

---
+++ src/components/CheckMyProjects/CheckMyProjects.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./CheckMyProjects.css";
+
+const CheckMyProjects = () => {
+  return <div className="checkMyProjects__body"></div>;
+};
+
+export default CheckMyProjects;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import AboutMe from "../../components/AboutMe/AboutMe";
 import WelcomeComponent from "../../components/WelcomeComponent/WelcomeComponent";
+import CheckMyProjects from "../../components/CheckMyProjects/CheckMyProjects";
 
 const Home = () => {
   return (
     <div>
       <WelcomeComponent />
       <AboutMe />
+      <CheckMyProjects />
     </div>
   );
 };
