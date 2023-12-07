a43c56ec0bb892e2bad506cae313133ff5175f2d
MSiesto <miguelsiesto@gmail.com>
2023-12-07 20:02:46 +0100

    updating sponsors name

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -19,15 +19,15 @@ const AboutMe = () => {
         alcanzado victorias sino que también he compartido mi pasión con
         entusiastas aspirantes, forjando una comunidad de hábiles conductores.
         Me enorgullece contar con el patrocinio de tres marcas innovadoras:
-        Sabway, Ambrosinni e-racing y Motorheads. Estas asociaciones van más
-        allá del respaldo financiero; representan un compromiso compartido con
-        la excelencia, la innovación y el progreso en el dinámico panorama de la
-        micromovilidad. Con Sabway, Ambrosinni e-racing y Motoheads a mi lado,
+        Sabway, Ambrosinni e-racing y Rhoxs. Estas asociaciones van más allá del
+        respaldo financiero; representan un compromiso compartido con la
+        excelencia, la innovación y el progreso en el dinámico panorama de la
+        micromovilidad. Con Sabway, Ambrosinni e-racing y Rhoxs a mi lado,
         navego por las corrientes de la revolución de la micromovilidad,
         empujando los límites de la velocidad, la sostenibilidad y la
-        emocionante adrenalina de la carretera abierta. Acompáñame en este
-        emocionante viaje mientras redefinimos el futuro de las carreras de
-        scooters eléctricos, una emocionante travesía a la vez."
+        emocionante adrenalina de la carretera abierta. Acompáñame en este viaje
+        mientras redefinimos el futuro del e-scooter competitivo, una
+        emocionante carrera a la vez."
       </p>
       <Button sx={{ my: 2, color: "white", display: "block" }}>
         <Link className="aboutme__link" to="/Contacto">
