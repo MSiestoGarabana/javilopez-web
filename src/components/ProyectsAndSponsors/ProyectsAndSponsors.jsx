daa88decc365b42ffaaab73ef9ca16d9b27d9327
MSiesto <miguelsiesto@gmail.com>
2023-12-07 20:03:36 +0100

    minor changes in proyects component

--- src/components/ProyectsAndSponsors/ProyectsAndSponsors.jsx
+++ src/components/ProyectsAndSponsors/ProyectsAndSponsors.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import "./ProyectsAndSponsors.css";
 
 const ProyectsAndSponsors = () => {
-  return <div className="h">h</div>;
+  return (
+    <div className="proyectsAndSponsors__body">
+      <div>proyects</div>
+      <div>sponsors</div>
+    </div>
+  );
 };
 
 export default ProyectsAndSponsors;
