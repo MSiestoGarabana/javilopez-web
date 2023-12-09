41289045e990299b2eb432239ee73d99a1fbfa38
MSiesto <miguelsiesto@gmail.com>
2023-12-09 23:04:46 +0100

    Sponsors component content

--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -1,26 +1,42 @@
 import React from "react";
-import "./Sponsors.css";
 import sponsorsImage from "../../media/sponsors.jpg";
 import sponsorsBackground from "../../media/sponsorsBackground.jpg";
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
+import "./Sponsors.css";
 
 //TODO: LOS NOMBRES DE ARCHIVO DE SPONSORS Y SPONSORSCARD DAN LUGAR A CONFUSIÓN
 const Sponsors = () => {
   return (
     <div className="sponsors__body">
       <div
-        className="sponsors__container--text"
+        className="sponsors__container--image"
         style={{
           backgroundImage: `url(${sponsorsImage})`,
           backgroundSize: `cover`,
+          backgroundPosition: "-170px",
         }}
       ></div>
       <div
-        className="sponsors__container--image"
+        className="sponsors__container--text"
         style={{
           backgroundImage: `url(${sponsorsBackground})`,
           backgroundSize: `cover`,
         }}
-      ></div>
+      >
+        <h2>
+          A lo largo de mi trayectoria en el mundo del e-scooter profesional he
+          tenido el placer de colaborar en diferentes proyectos. He escogido
+          participar en ellos porque creo que ayudan al scooter eléctrico a
+          apuntar hacia un futuro más profesional, competitivo y sostenible.
+        </h2>
+        <h2>Puedes encontrarlos aquí</h2>
+        <Button sx={{ my: 2, color: "white", display: "block" }}>
+          <Link className="proyects__link" to="/Sponsors">
+            Sponsors
+          </Link>
+        </Button>
+      </div>
     </div>
   );
 };
