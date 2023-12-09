2583e4a1012fdba2bc7356d8bbf11bab8af6688d
MSiesto <miguelsiesto@gmail.com>
2023-12-09 23:04:38 +0100

    Proyects component content

--- src/components/Proyects/Proyects.jsx
+++ src/components/Proyects/Proyects.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import proyectsImage from "../../media/_MMS3220.jpg";
 import proyectsBackgroundImage from "../../media/Riding.jpg";
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
+
 import "./Proyects.css";
 
 const Proyects = () => {
@@ -13,7 +16,18 @@ const Proyects = () => {
           backgroundSize: "cover",
         }}
       >
-        proyects
+        <h2>
+          A lo largo de mi trayectoria en el mundo del e-scooter profesional he
+          tenido el placer de colaborar en diferentes proyectos. He escogido
+          participar en ellos porque creo que ayudan al scooter eléctrico a
+          apuntar hacia un futuro más profesional, competitivo y sostenible.
+        </h2>
+        <h2>Puedes encontrarlos aquí</h2>
+        <Button sx={{ my: 2, color: "white", display: "block" }}>
+          <Link className="proyects__link" to="/Proyectos">
+            Proyectos
+          </Link>
+        </Button>
       </div>
       <div
         className="proyects__container--image"
