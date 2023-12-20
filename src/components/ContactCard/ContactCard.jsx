9835bf86f1630ad3234878e776882cbc49f689d1
MSiesto <miguelsiesto@gmail.com>
2023-12-20 23:33:20 +0100

    styling contactCard

--- src/components/ContactCard/ContactCard.jsx
+++ src/components/ContactCard/ContactCard.jsx
@@ -8,13 +8,30 @@ const ContactCard = () => {
   return (
     <Card className="contactCard__body">
       <div className="contactCard__mailCard">
-        <MailIcon />
+        <MailIcon
+          sx={{
+            width: "3em",
+            height: "3em",
+          }}
+          className="contactCard__icon"
+        />
         <p>javiespinosa@sabway.es</p>
       </div>
       <div className="contactCard__instaCard">
-        <InstagramIcon />
-        <a href="https://www.instagram.com/javitolopezz_" target="_blank">
-          My instagram
+        <a
+          href="https://www.instagram.com/javitolopezz_"
+          target="_blank"
+          className="contactCard__instaLink"
+        >
+          <InstagramIcon
+            sx={{
+              width: "3em",
+              height: "3em",
+              color: "black",
+            }}
+            className="contactCard__icon"
+          />
+          <p className="contactCard__instaText">My instagram</p>
         </a>
       </div>
     </Card>
